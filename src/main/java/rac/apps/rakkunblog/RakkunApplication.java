b0192cc1a6bfbf5e800fcfcf88c89c870a002e73
Silviu Oprea <silviu.oprea17@yahoo.com>
2023-08-07 19:48:43 +0300

    Basic Spring Boot Configuration for a Blog Application

---
+++ src/main/java/rac/apps/rakkunblog/RakkunApplication.java
@@ -0,0 +1,13 @@
+package rac.apps.rakkunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakkunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakkunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rac/apps/rakkunblog/RakkunApplicationTests.java
@@ -0,0 +1,13 @@
+package rac.apps.rakkunblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakkunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
